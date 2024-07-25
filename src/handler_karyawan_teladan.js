ffc2865a87c3a03479c7de88fa7a0dd78b43849d
Isnanadifatul <109615073+Isnanadifatul@users.noreply.github.com>
2024-07-25 10:17:13 +0700

    Update handler_karyawan_teladan.js

--- src/handler_karyawan_teladan.js
+++ src/handler_karyawan_teladan.js
@@ -5,7 +5,7 @@ const { connection } = require('../db-config/connect');
 //insert survey1
 async function insertSurveyHandler(request, h) {
   try {
-      const {nama_lengkap, jenis_kelamin, umur, pendidikan, masa_kerja, nama_kandidat,
+      const {nama_lengkap, jenis_kelamin, umur, pendidikan, masa_kerja, nomor_kandidat, nama_kandidat,
         pertanyaan_1, pertanyaan_2, pertanyaan_3, pertanyaan_4, pertanyaan_5, 
         pertanyaan_6, pertanyaan_7, pertanyaan_8, pertanyaan_9, pertanyaan_10,
         pertanyaan_11, pertanyaan_12, pertanyaan_13, pertanyaan_14, pertanyaan_15,
@@ -13,7 +13,7 @@ async function insertSurveyHandler(request, h) {
         pertanyaan_21} = request.payload;
 
       // Insert user baru
-      await insertUser(nama_lengkap, jenis_kelamin, umur, pendidikan, masa_kerja, nama_kandidat,
+      await insertUser(nama_lengkap, jenis_kelamin, umur, pendidikan, masa_kerja, nomor_kandidat, nama_kandidat,
         pertanyaan_1, pertanyaan_2, pertanyaan_3, pertanyaan_4, pertanyaan_5, 
         pertanyaan_6, pertanyaan_7, pertanyaan_8, pertanyaan_9, pertanyaan_10,
         pertanyaan_11, pertanyaan_12, pertanyaan_13, pertanyaan_14, pertanyaan_15,
@@ -55,7 +55,7 @@ async function insertSurveyHandler(request, h) {
                 [Sequelize.fn('AVG', Sequelize.col('pertanyaan_20')), 'pertanyaan_20'],
                 [Sequelize.fn('AVG', Sequelize.col('pertanyaan_21')), 'pertanyaan_21']
       ],
-      group: ['nama_kandidat']
+      group: ['nomor_kandidat']
   });
 
   if (result.length === 0) {
@@ -73,60 +73,81 @@ async function insertSurveyHandler(request, h) {
 }
 };
 
-//Nilai rata rata dikali 30%
-const AVGConvert30 = async (request, h) => {
+//get rata rata skor
+const AVGSurveyPerKandidat = async (request, h) => {
   try {
       const results = await survey_pegawai_teladan.findAll({
           attributes: [
               'nama_kandidat',
               [Sequelize.fn('AVG', Sequelize.literal(`
-                  
-                      (pertanyaan_1 + pertanyaan_2 + pertanyaan_3 + pertanyaan_4 + pertanyaan_5 + 
-                       pertanyaan_6 + pertanyaan_7 + pertanyaan_8 + pertanyaan_9 + pertanyaan_10 + 
-                       pertanyaan_11 + pertanyaan_12 + pertanyaan_13 + pertanyaan_14 + pertanyaan_15 + 
-                       pertanyaan_16 + pertanyaan_17 + pertanyaan_18 + pertanyaan_19 + pertanyaan_20 + 
-                       pertanyaan_21) / 21
-                  
-              `)), 'avg_total_hasil_30_percent']
+                  (pertanyaan_1 + pertanyaan_2 + pertanyaan_3 + pertanyaan_4 + pertanyaan_5 + 
+                   pertanyaan_6 + pertanyaan_7 + pertanyaan_8 + pertanyaan_9 + pertanyaan_10 + 
+                   pertanyaan_11 + pertanyaan_12 + pertanyaan_13 + pertanyaan_14 + pertanyaan_15 + 
+                   pertanyaan_16 + pertanyaan_17 + pertanyaan_18 + pertanyaan_19 + pertanyaan_20 + 
+                   pertanyaan_21) / 21
+              `)), 'avg_total_hasil']
           ],
-          group: ['nama_kandidat'],
-          raw: true
+          group: ['nomor_kandidat']
       });
 
       if (results.length === 0) {
           return h.response({ error: 'Data not found' }).code(404);
       }
 
-      for (const result of results) {
-          const avgTotal = result.avg_total_hasil_30_percent;
-          console.log(`Menyimpan hasil untuk kandidat ${result.nama_kandidat}: ${avgTotal}`);
-
-          if (avgTotal !== undefined) {
-              await saveKonversi(result.nama_kandidat, avgTotal);
-          } else {
-              console.warn(`Nilai untuk ${result.nama_kandidat} adalah undefined`);
-          }
-      }
-
       return h.response(results).code(200);
   } catch (err) {
-      console.error('Failed to fetch data:', err);
+      console.error(err);
       return h.response({ error: 'Failed to fetch data' }).code(500);
   }
 };
 
-//
-// const handleSaveResponse = async (request, h) => {
-//   try {
-//       const { nama_kandidat, konversi_nilai } = request.payload;
-//       await saveKonversi(nama_kandidat, konversi_nilai);
-//       return h.response({ message: 'Response berhasil disimpan' }).code(201);
-//   } catch (error) {
-//       console.error('Error:', error);
-//       return h.response({ error: 'Gagal menyimpan response' }).code(500);
-//   }
-// };
-//get survey tiap kandidat
+//Nilai rata rata dikali 30%
+const AVGConvert30 = async (request, h) => {
+  try {
+      const results = await survey_pegawai_teladan.findAll({
+        attributes: [
+            'nomor_kandidat',
+            'nama_kandidat',
+            [Sequelize.fn('ROUND', Sequelize.literal(`
+                AVG(
+                    (pertanyaan_1 + pertanyaan_2 + pertanyaan_3 + pertanyaan_4 + pertanyaan_5 + 
+                     pertanyaan_6 + pertanyaan_7 + pertanyaan_8 + pertanyaan_9 + pertanyaan_10 + 
+                     pertanyaan_11 + pertanyaan_12 + pertanyaan_13 + pertanyaan_14 + pertanyaan_15 + 
+                     pertanyaan_16 + pertanyaan_17 + pertanyaan_18 + pertanyaan_19 + pertanyaan_20 + 
+                     pertanyaan_21) / 21
+                )
+            `), 2), 'avg_total_hasil_30_percent']
+        ],
+        group: ['nomor_kandidat', 'nama_kandidat'],
+        raw: true
+    });
+
+    if (results.length === 0) {
+        return h.response({ error: 'Data not found' }).code(404);
+    }
+
+    for (const result of results) {
+        const avgTotal = result.avg_total_hasil_30_percent;
+        const nomorKandidat = result.nomor_kandidat;
+        const namaKandidat = result.nama_kandidat;
+
+        console.log(`Menyimpan hasil untuk kandidat ${nomorKandidat}: ${avgTotal}`);
+
+        if (avgTotal !== undefined) {
+            await saveKonversi(nomorKandidat, namaKandidat, avgTotal);
+        } else {
+            console.warn(`Nilai untuk ${nomorKandidat} adalah undefined`);
+        }
+    }
+
+    return h.response(results).code(200);
+} catch (err) {
+    console.error('Failed to fetch data:', err);
+    return h.response({ error: 'Failed to fetch data' }).code(500);
+}
+};
+
+
 
 const getSurveyKandidat1 = async (request, h) => {
   try {
@@ -140,7 +161,7 @@ const getSurveyKandidat1 = async (request, h) => {
           'pertanyaan_21'
       ],
           where: {
-              nama_kandidat: 'Kandidat 1'
+            nomor_kandidat: 'Kandidat 1'
           }
       });
 
@@ -166,7 +187,7 @@ const getSurveyKandidat2 = async (request, h) => {
           'pertanyaan_21'
       ],
           where: {
-              nama_kandidat: 'Kandidat 2'
+            nomor_kandidat: 'Kandidat 2'
           }
       });
 
@@ -193,7 +214,7 @@ const getSurveyKandidat3 = async (request, h) => {
           'pertanyaan_21'
       ],
           where: {
-              nama_kandidat: 'Kandidat 3'
+            nomor_kandidat: 'Kandidat 3'
           }
       });
 
@@ -214,6 +235,6 @@ const getSurveyKandidat3 = async (request, h) => {
     getSurveyKandidat1,
     getSurveyKandidat2,
     getSurveyKandidat3,
+    AVGSurveyPerKandidat,
     AVGConvert30
     };
-
